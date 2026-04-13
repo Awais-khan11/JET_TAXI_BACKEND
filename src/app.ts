16c1390bc667a67fd11eebb8517463c33ae8b53e
rajaaaaa <157604047+nouman308379@users.noreply.github.com>
2026-04-14 00:14:00 +0500

    fix

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions: cors.CorsOptions = {
-  origin: ["*"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
